65bf67549bf469dd8131fe6e9f0f786771bee202
pk222fe <pk222fe@student.lnu.se>
2014-01-16 12:54:51 +0100

    Skriv klart sprint

---
+++ Members/Members/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Members
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'database1DataSet.Members' table. You can move, or remove it, as needed.
+            this.membersTableAdapter.Fill(this.database1DataSet.Members);
+
+        }
+
+        private void membersBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.membersBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.database1DataSet);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("");
+        }
+    }
+}
